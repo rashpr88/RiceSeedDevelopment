4e93a74c9652b8981d40013d87ad80dfd48942ae
LAPTOP-7SBJP7PM\Rashini De Silva <rashpr88@gmail.com>
2022-12-06 13:17:32 +0530

    commit

--- algorithms.py
+++ algorithms.py
@@ -86,7 +86,7 @@ def predict (ngraph,seeds,d,diff):
         x = 0
 
         for node in graph.nodes:  # assigning func score to each node
-            graph.nodes[node]['functional_score'] = en_fluid[n, 0]
+            graph.nodes[node]['functional_score'] = en_fluid[x,0]
             x += 1
 
         return graph
